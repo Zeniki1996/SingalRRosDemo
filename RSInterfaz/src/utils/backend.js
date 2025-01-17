1354172880e3513c8df1b0f68e94448c097d15b1
Nikita Martínez <62626494+Zeniki1996@users.noreply.github.com>
2025-01-17 14:17:23 -0500

    HUB y imprimir key

--- RSInterfaz/src/utils/backend.js
+++ RSInterfaz/src/utils/backend.js
@@ -4,6 +4,8 @@ import { OpenAI } from "openai";
 
 //Conexion chatGPT
 console.log("Variable de entorno viendo si funca",process.env);
+console.log("BACKEND_URL:", process.env.REACT_APP_BACKEND_URL);
+console.log("OPENAI_API_KEY:", process.env.REACT_APP_OPENAI_API_KEY);
 const openai = new OpenAI({
   apiKey: process.env.REACT_APP_OPENAI_API_KEY, // Asegúrate de tener la clave configurada prueba
   dangerouslyAllowBrowser: true,
